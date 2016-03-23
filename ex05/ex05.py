a3cfd66e7f2377f5de0d9a492ea044aa7a42a691
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-03-23 18:18:53 +0900

    added ex05.py

---
+++ ex05/ex05.py
@@ -0,0 +1,19 @@
+my_name = 'Kangwon Lee'
+my_age = 46  # It's a lie
+my_height_cm = 180
+my_weight_kg = 80
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d centimeters tall." % my_height_cm
+print "He's %d kilograms heavy." % my_weight_kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg
+)
