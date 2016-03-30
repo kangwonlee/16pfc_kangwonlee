d4f350a300082f44ca19e5740270dc2a4e46adb6
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-03-30 19:08:18 +0900

    Study Drills 10.02

--- ex10/ex10.py
+++ ex10/ex10.py
@@ -3,12 +3,12 @@ tabby_cat2 = "    I'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 # TODO : Escape Sequence
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print(tabby_cat)
 print(tabby_cat2)
