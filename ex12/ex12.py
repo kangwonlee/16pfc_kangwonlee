0903aae396ba6fcab08916b6fb47ec45b9545922
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-06 17:51:15 +0900

    ex12 # http://learnpythonthehardway.org/book/ex12.html

---
+++ ex12/ex12.py
@@ -0,0 +1,8 @@
+# -*-coding:cp949
+# http://learnpythonthehardway.org/book/ex12.html
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print("So, you're %r old, %r tall and %r heavy" % (
+    age, height, weight))
