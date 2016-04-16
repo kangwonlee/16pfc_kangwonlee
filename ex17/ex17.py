edff7b81cba6c4240fff663f47fab8cf9c310c08
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-04-16 13:56:26 +0900

    python 2 3 ex17

--- ex17/ex17.py
+++ ex17/ex17.py
@@ -1,5 +1,5 @@
-from sys import argv
 from os.path import exists
+from sys import argv
 
 script, from_file, to_file = argv
 
@@ -9,16 +9,16 @@ print("Copying from %s to %s" % (from_file, to_file))
 in_file = open(from_file)
 indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)
+print("The input file is %d bytes long" % len(indata))
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+print("Does the output file exist? %r" % exists(to_file))
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print "Alright, all done."
+print("Alright, all done.")
 
 out_file.close()
 in_file.close()
