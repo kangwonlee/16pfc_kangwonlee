d2afa998b8fff212b03494bbf9ce7dd3e06885ea
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-04-23 02:59:16 +0900

    ex01.py additional instructions

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex1.html
+
 print("Hello World")
 print("Hello Again")
 print("I like typing this.")
@@ -9,3 +12,9 @@ print('I "said" do not touch this.')
 print("Kangwon Lee")
 print("2016 03 09 19:49")
 print("Lee Se Dol : Alpha Go = 0:1")
+
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
