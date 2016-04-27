2f51f9027accc1cdea2217d8504f9556c7e28ae7
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-27 18:25:59 +0900

    refactored upper coordinates

--- acad/acad_boring.py
+++ acad/acad_boring.py
@@ -9,14 +9,7 @@ def main():
     for di_mm in inner_diameters_mm:
         inner_radius_mm.append(di_mm * 0.5)
 
-    upper_coordinates_mm = [(length_mm, outer_radius_mm),
-                            (0.0, outer_radius_mm),
-                            ]
-    x = 0.0
-    for ri_mm, section_length_mm in zip(inner_radius_mm, section_lengths_mm):
-        upper_coordinates_mm.append((x, ri_mm))
-        x += section_length_mm
-        upper_coordinates_mm.append((x, ri_mm))
+    upper_coordinates_mm = build_upper_coordinates(inner_radius_mm, length_mm, outer_radius_mm, section_lengths_mm)
 
     print upper_coordinates_mm
 
@@ -30,6 +23,19 @@ def main():
         f.write('hatch k ')
         f.write('%g,%g' % (xy[0]-0.1, xy[0]+0.1, ))
         f.close()
+
+
+def build_upper_coordinates(inner_radius_mm, length_mm, outer_radius_mm, section_lengths_mm):
+    upper_coordinates_mm = [(length_mm, outer_radius_mm),
+                            (0.0, outer_radius_mm),
+                            ]
+    x = 0.0
+    for ri_mm, section_length_mm in zip(inner_radius_mm, section_lengths_mm):
+        upper_coordinates_mm.append((x, ri_mm))
+        x += section_length_mm
+        upper_coordinates_mm.append((x, ri_mm))
+    return upper_coordinates_mm
+
 
 if __name__ == '__main__':
     main()
