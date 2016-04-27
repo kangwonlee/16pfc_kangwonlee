9c00ec40981168a41db3243526a9b0292e22e842
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-27 18:22:57 +0900

    write command drawing upper part

--- acad/acad_boring.py
+++ acad/acad_boring.py
@@ -1,7 +1,7 @@
 outer_diameter_mm = 50
 inner_diameters_mm = (40, 20, 30, 20, 10)
-length_mm = 100
 section_lengths_mm = (15, 20, 15, 20, 30)
+length_mm = sum(section_lengths_mm)
 
 outer_radius_mm = outer_diameter_mm * 0.5
 inner_radius_mm = []
@@ -18,3 +18,12 @@ for ri_mm, section_length_mm in zip(inner_radius_mm, section_lengths_mm):
     upper_coordinates_mm.append((x, ri_mm))
 
 print upper_coordinates_mm
+
+with open('boring.scr', 'w') as f:
+    f.write('line')
+    f.write(' ')
+    for xy in upper_coordinates_mm:
+        f.write('%g,%g' % xy)
+        f.write(' ')
+    f.write('c\n')
+    f.close()
