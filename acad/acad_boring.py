cc5097748117e2e4d2fa0a26bf238970847f71b3
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-27 18:25:07 +0900

    everything -> main() function added hatching

--- acad/acad_boring.py
+++ acad/acad_boring.py
@@ -1,29 +1,35 @@
-outer_diameter_mm = 50
-inner_diameters_mm = (40, 20, 30, 20, 10)
-section_lengths_mm = (15, 20, 15, 20, 30)
-length_mm = sum(section_lengths_mm)
+def main():
+    outer_diameter_mm = 50
+    inner_diameters_mm = (40, 20, 30, 20, 10)
+    section_lengths_mm = (15, 20, 15, 20, 30)
+    length_mm = sum(section_lengths_mm)
 
-outer_radius_mm = outer_diameter_mm * 0.5
-inner_radius_mm = []
-for di_mm in inner_diameters_mm:
-    inner_radius_mm.append(di_mm * 0.5)
+    outer_radius_mm = outer_diameter_mm * 0.5
+    inner_radius_mm = []
+    for di_mm in inner_diameters_mm:
+        inner_radius_mm.append(di_mm * 0.5)
 
-upper_coordinates_mm = [(length_mm, outer_radius_mm),
-                        (0.0, outer_radius_mm),
-                        ]
-x = 0.0
-for ri_mm, section_length_mm in zip(inner_radius_mm, section_lengths_mm):
-    upper_coordinates_mm.append((x, ri_mm))
-    x += section_length_mm
-    upper_coordinates_mm.append((x, ri_mm))
+    upper_coordinates_mm = [(length_mm, outer_radius_mm),
+                            (0.0, outer_radius_mm),
+                            ]
+    x = 0.0
+    for ri_mm, section_length_mm in zip(inner_radius_mm, section_lengths_mm):
+        upper_coordinates_mm.append((x, ri_mm))
+        x += section_length_mm
+        upper_coordinates_mm.append((x, ri_mm))
 
-print upper_coordinates_mm
+    print upper_coordinates_mm
 
-with open('boring.scr', 'w') as f:
-    f.write('line')
-    f.write(' ')
-    for xy in upper_coordinates_mm:
-        f.write('%g,%g' % xy)
+    with open('boring.scr', 'w') as f:
+        f.write('line')
         f.write(' ')
-    f.write('c\n')
-    f.close()
+        for xy in upper_coordinates_mm:
+            f.write('%g,%g' % xy)
+            f.write(' ')
+        f.write('c\n')
+        f.write('hatch k ')
+        f.write('%g,%g' % (xy[0]-0.1, xy[0]+0.1, ))
+        f.close()
+
+if __name__ == '__main__':
+    main()
