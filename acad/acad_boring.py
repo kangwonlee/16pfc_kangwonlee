ef636f2c79b265738d3225202f8ca997993e29ed
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-27 19:37:05 +0900

    tried hatch ?
    명령: hatch
    패턴 이름 입력 또는 [?/솔리드(S)/사용자 정의(U)/그라데이션(G)] <ANSI31>: ?
    나열할 패턴(들) 입력 <*>: ANSI31
    ANSI31          -  ANSI 철, 벽돌, 돌쌓기

--- acad/acad_boring.py
+++ acad/acad_boring.py
@@ -34,7 +34,7 @@ def write_upper_section(f, upper_coordinates_mm):
 
 
 def hatch_upper(f, upper_coordinates_mm):
-    f.write('hatch ANSI31 10 0 k ')
+    f.write('hatch ? ANSI31 10 0 k ')
     f.write('%g,%g' % (upper_coordinates_mm[-1][0] - 0.1, upper_coordinates_mm[-1][1] + 0.1))
     f.write(' \n')
 
