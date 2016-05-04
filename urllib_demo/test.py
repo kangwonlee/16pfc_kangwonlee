ce1a5389cc5d2e27ed25a754952b6876f83049e5
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-04 19:23:17 +0900

    urllib demo : added comments

--- urllib_demo/test.py
+++ urllib_demo/test.py
@@ -1,16 +1,23 @@
+# -*-coding:utf8
 import urllib
 import bs4
 
 url = "http://photo.naver.com/camera/"
 
+# website 에서 정보를 가져 옴
 website = urllib.urlopen(url)
 text = website.read()
 website.close()
 
+# 가져 온 정보를 해석함
 soup = bs4.BeautifulSoup(text, 'lxml')
+
+# 해석한 정보에서 img 포함된 내용을 추출함
 imgs = soup.find_all('img')
 
+# 추출한 내용을 하나씩 살펴 봄
 for image_info in imgs:
     print(image_info.get('src'))
+    print(urllib.urlretrieve(image_info.get('src')))
 
 # urllib.urlretrieve()
