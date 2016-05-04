3f98b63eab09c9fbf4752f2a3d4d606ef88a70a1
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-04 17:50:52 +0900

    ex24 additional instructions (wk09)

--- ex24/ex24.py
+++ ex24/ex24.py
@@ -36,3 +36,11 @@ start_point = start_point / 10
 
 print("We can also do that this way:")
 print("We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point))
+
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 오류노트 에 각자 오류노트 작성
