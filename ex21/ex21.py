5989603fb9b6fb1aeba2d8175a33efa8d6d85683
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-10 10:45:15 +0900

    added ex21

---
+++ ex21/ex21.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex21.html
+
+
+def add(a, b):
+    print("ADDING %d + %d" % (a, b))
+    return a + b
+
+
+def subtract(a, b):
+    print("SUBTRACTING %d - %d" % (a, b))
+    return a - b
+
+
+def multiply(a, b):
+    print("MULTIPLYING %d * %d" % (a, b))
+    return a * b
+
+
+def divide(a, b):
+    print("DIVIDING %d / %d" % (a, b))
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print("Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
+
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 오류노트 에 각자 오류노트 작성
