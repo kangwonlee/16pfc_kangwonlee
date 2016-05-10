5c8430297e2d97b48733adab9bf5f1c914e7ab5f
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-10 10:37:47 +0900

    ex20 additional instruction

--- ex20/ex20.py
+++ ex20/ex20.py
@@ -1,4 +1,6 @@
-# -*-coding:utf8
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex20.html
+
 from sys import argv
 
 script, input_file = argv
@@ -35,3 +37,11 @@ print_a_line(current_line, current_file)
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 오류노트 에 각자 오류노트 작성
