234f0bd7cf16b147c4d7c2b626a156444fa68bf9
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-14 10:44:09 +0900

    ex28 ()'s

--- ex28/ex28.py
+++ ex28/ex28.py
@@ -1,18 +1,18 @@
 # coding: utf-8
 # 참고문헌: http://learnpythonthehardway.org/book/ex28.html
 
-print(" 1. True and True = %s" % True and True)
-print(" 2. False and True = %s" % False and True)
+print(" 1. True and True = %s" % (True and True))
+print(" 2. False and True = %s" % (False and True))
 print(" 3. 1==1 and 2==1 = %s" % ((1 == 1) and (2 == 1)))
-print(" 4. 'test' == 'test' = %s" % 'test' == 'test')
+print(" 4. 'test' == 'test' = %s" % ('test' == 'test'))
 print(" 5. 1==1 or 2!=1 = %s " % ((1 == 1) or (2 != 1)))
-print(" 6. True and 1==1 = %s" % True and (1 == 1))
-print(" 7. False and 0!=0 = %s" % False and (0 != 0))
+print(" 6. True and 1==1 = %s" % (True and (1 == 1)))
+print(" 7. False and 0!=0 = %s" % (False and (0 != 0)))
 print(" 8. True or 1==1 = %s" % True or (1 == 1))
-print(" 9. 'test' == 'testing' = %s" % 'test' == 'testing')
-print("10. 1!=0 and 2==1 = %s" % (1 != 0) and (2 == 1))
-print("11. 'test'!='testing' = %s" % 'test' != 'testing')
-print("12. 'test'==1 = %s" % 'test' == 1)
+print(" 9. 'test' == 'testing' = %s" % ('test' == 'testing'))
+print("10. 1!=0 and 2==1 = %s" % ((1 != 0) and (2 == 1)))
+print("11. 'test'!='testing' = %s" % ('test' != 'testing'))
+print("12. 'test'== 1 = %s" % ('test' == 1))
 print("13. not(True and False) = %s" % (not (True and False)))
 print("14. not(1==1 and 0!=1) = %s" % (not ((1 == 1) and (0 != 1))))
 print("15. not(10==1 or 1000==1000) = %s " % (not ((10 == 1) or (1000 == 1000))))
@@ -26,10 +26,10 @@ print("19. 'chunky'=='bacon' and not (3==4 or 3==3) = %s" % (
 print("20. 3==3 and not('testing'=='testing' or 'Python'=='Fun') = %s" % (
     (3 == 3) and not (('testing' == 'testing') or ('Python' == 'Fun'))))
 
-print("extra 1. 'test' and 'test' = = %s" % 'test' and 'test')
-print("extra 2. 1 and 1 = = %s" % 1 and 1)
-print("extra 3. False and 1 = = %s" % False and 1)
-print("extra 4. True and 1 = = %s" % True and 1)
+print("extra 1. 'test' and 'test' = = %s" % ('test' and 'test'))
+print("extra 2. 1 and 1 = = %s" % (1 and 1))
+print("extra 3. False and 1 = = %s" % (False and 1))
+print("extra 4. True and 1 = = %s" % (True and 1))
 
 # 여기까지 입력 후 add, commit
 
