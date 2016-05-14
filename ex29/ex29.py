7acba289cfca9de01ca66b078a6c46642cd3a224
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-14 11:07:53 +0900

    added ex29

---
+++ ex29/ex29.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex29.html
+
+people = 10
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cast! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
+
+# 여기까지 입력 후 add, commitS
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 오류노트 에 각자 오류노트 작성
