3b6335287be45a0b8aabee16ef70d8095fae1a14
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-17 11:41:53 +0900

    ex32 building lists : append each element

--- ex32/ex32.py
+++ ex32/ex32.py
@@ -21,6 +21,12 @@ for i in change:
 # we can also build lists, first start with an empty one
 elements = []
 
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print(" Adding %d to the list." % i)
+    # append is a function that lists understand
+    elements.append(i)
+
 # 여기까지 입력 후 add, commitS
 
 # 각 행 주석 입력 후 commit
