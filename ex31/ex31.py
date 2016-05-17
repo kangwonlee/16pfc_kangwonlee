dcc2a07c26df9566633d4bcf8eeea49553f23231
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-17 10:41:58 +0900

    start ex31

---
+++ ex31/ex31.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex31.html
+
+
+# 여기까지 입력 후 add, commitS
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 오류노트 에 각자 오류노트 작성
