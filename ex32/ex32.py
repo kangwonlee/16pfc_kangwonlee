a449be7df8e88bdd76360e9cb24ca5e4379d0806
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-17 11:37:27 +0900

    ex32 initialize lists : the_count, fruits, change

--- ex32/ex32.py
+++ ex32/ex32.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 # 참고문헌: http://learnpythonthehardway.org/book/ex32.html
 
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 # 여기까지 입력 후 add, commitS
 
