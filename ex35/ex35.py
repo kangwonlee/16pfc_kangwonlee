737edf43cf4a37e29932e59357d816a01c5020b9
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-24 10:01:11 +0900

    ex25 gold_room()

--- ex35/ex35.py
+++ ex35/ex35.py
@@ -1,5 +1,22 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex34.html
+from sys import exit
+
+
+def gold_room():
+    print("This room is full of gold. How much do you take?")
+
+    choice = raw_input("> ")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number.")
+
+    if 50 > how_much:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
 
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
