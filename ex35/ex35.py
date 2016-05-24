a0f26e2205e01d5fea8e7d458f01fd4fce1cd174
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-24 10:12:30 +0900

    ex25 dead() function

--- ex35/ex35.py
+++ ex35/ex35.py
@@ -57,6 +57,11 @@ def cthulhu_room():
         cthulhu_room()
 
 
+def dead(why):
+    print("%s, Good job!" % why)
+    exit(0)
+
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
