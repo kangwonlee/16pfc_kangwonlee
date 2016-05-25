3c833ad20f88032479cee37c36a3fe7e19f651a0
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 20:11:44 +0900

    ex39_test.py : abbreviations - cities

--- ex39/ex39_test.py
+++ ex39/ex39_test.py
@@ -11,6 +11,16 @@ hashmap.set(states, 'California', 'CA')
 hashmap.set(states, 'New York', 'NY')
 hashmap.set(states, 'Michigan', 'MI')
 
+# create a basic set of states and some cities in them
+cities = hashmap.new()
+hashmap.set(cities, 'CA', 'Los Angeles')
+hashmap.set(cities, 'HI', 'Honolulu')
+hashmap.set(cities, 'FL', 'Orlando')
+
+# add some more cities
+hashmap.set(cities, 'NY', 'New York')
+hashmap.set(cities, 'OR', 'Portland')
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
