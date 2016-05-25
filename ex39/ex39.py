2edc6c55aba0eee60c738a64fd0b88d9b350a9ad
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 19:16:42 +0900

    ex39 : cities dictionary

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -10,6 +10,18 @@ states = {
     'Michigan': 'MI'
 }
 
+# create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
