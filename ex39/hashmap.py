2a934a688ff2eab19c02e63d0a222a3c25e90033
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 19:44:21 +0900

    hashmap.py : started new()

--- ex39/hashmap.py
+++ ex39/hashmap.py
@@ -2,6 +2,13 @@
 # http://learnpythonthehardway.org/book/ex39.html
 
 
+def new(num_buckets=256):
+    """
+    Initializes a Map with the given number of buckets.
+    :param num_buckets:
+    :return:
+    """
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
