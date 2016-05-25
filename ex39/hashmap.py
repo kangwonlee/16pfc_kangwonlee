0a780f56813c2f8faed8ed1deec7d376168aaf01
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 19:47:21 +0900

    hashmap.py : added hash_key()

--- ex39/hashmap.py
+++ ex39/hashmap.py
@@ -14,6 +14,16 @@ def new(num_buckets=256):
     return aMap
 
 
+def hash_key(aMap, key):
+    """
+    Given a key this will create a number and then convert it to an index for the aMap's buckets.
+    :param aMap:
+    :param key:
+    :return:
+    """
+    return hash(key) % len(aMap)
+
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
