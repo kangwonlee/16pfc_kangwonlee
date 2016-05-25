da75ae3d990272bbaf3124be1e7000ba4ba87cf1
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 19:22:39 +0900

    ex39 : print out all cities

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -36,6 +36,11 @@ print('-' * 10)
 for state, abbrev in states.items():
     print("%s is abbreviated %s" % (state, abbrev))
 
+# print every city in state
+print('-' * 10)
+for abbrev, city in cities.items():
+    print("%s has the city %s" % (abbrev, city))
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
