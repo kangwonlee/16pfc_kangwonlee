88eb2e491e5575d803037635538e800005c107c4
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 17:59:52 +0900

    finished entering ex38 example

--- ex38/ex38.py
+++ ex38/ex38.py
@@ -1,5 +1,26 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex38.html
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while (len(stuff)) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: %s" % next_one)
+    stuff.append(next_one)
+    print("There as %d times now." % len(stuff))
+
+print("There we go :%s" % str(stuff))
+print("Let's do some things with stuff")
+
+print("stuff[1] = %s" % stuff[1])
+print("stuff[-1] = %s" % stuff[-1])
+print("stuff.pop() = %s" % stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
 
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
