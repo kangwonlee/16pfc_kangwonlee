4244f0dec69dd693b156e49840326882a9b9f8ec
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-25 20:21:37 +0900

    added main part

--- ex39/ex39_test.py
+++ ex39/ex39_test.py
@@ -36,6 +36,25 @@ print('-' * 10)
 print('Hawaii has: %s' % hashmap.get(cities, hashmap.get(states, 'Hawaii')))
 print('Florida has: %s' % hashmap.get(cities, hashmap.get(states, 'Florida')))
 
+# print every state abbreviation
+print('-' * 10)
+hashmap.list(states)
+
+# print every city in state
+print('-' * 10)
+hashmap.list(cities)
+
+print('-' * 10)
+state = hashmap.get(states, 'Texas')
+
+if not state:
+    print("Sorry, no Texas")
+
+# default values using | | = with the nill result
+# can you do this on one line?
+city = hashmap.get(cities, 'Tx', 'Does Not Exist')
+print("The city for the state 'TX' is: %s" % city)
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
