034cd2ec1a3344b1b9f747745aedf50d823724a0
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-05-30 00:34:36 +0900

    ex47 ex47.py : added test_room()

--- ex47/ex47.py
+++ ex47/ex47.py
@@ -2,6 +2,19 @@
 # 참고문헌: http://learnpythonthehardway.org/book/ex47.html
 # 각 행 주석 입력 후 commit
 
+from nose.tools import *
+
+from game import Room
+
+
+def test_room():
+    gold = Room("GoldRoom",
+                """This room has gold in it you can grab. There's a
+door to the north.""")
+
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
+
 # 각자 Study drills 시도 후 필요시 commit
 
 # 열린게시판 / 오류노트 에 각자 오류노트 작성
