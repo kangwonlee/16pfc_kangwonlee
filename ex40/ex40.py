6edd1290cffb8a3fc4238d03cbad23865ea7fe9e
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-05-29 23:31:47 +0900

    ex40 : added MyStuff class

--- ex40/ex40.py
+++ ex40/ex40.py
@@ -9,6 +9,21 @@ import mystuff
 mystuff.apple()
 print(mystuff.tangerine)
 
+
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+
+# end class MyStuff
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
