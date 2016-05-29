1c8aa7f823c315a2afc968195876485a64264726
KangWon LEE <kangwon_fwd@naver.com>
2016-05-29 17:43:34 +0900

    ex40 dictionary style

--- ex40/ex40.py
+++ ex40/ex40.py
@@ -1,6 +1,9 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex40.html
 
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
