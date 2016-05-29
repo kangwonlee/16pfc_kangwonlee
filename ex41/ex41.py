53d4d2ad44d7ed6ec442b47d545f38f9f7740b79
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-05-29 18:22:36 +0900

    start ex41

---
+++ ex41/ex41.py
@@ -0,0 +1,9 @@
+# coding: utf8
+# 참고문헌: http://learnpythonthehardway.org/book/ex41.html
+#   https://wikidocs.net/27
+# 각 행 주석 입력 후 commit
+
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 열린게시판 / 오류노트 에 각자 오류노트 작성
