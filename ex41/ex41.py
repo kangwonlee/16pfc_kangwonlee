0313569eb94e6d09666a391ca22a4f8d2e66807d
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-05-29 18:24:00 +0900

    ex41 words and phrases

--- ex41/ex41.py
+++ ex41/ex41.py
@@ -3,6 +3,27 @@
 #   https://wikidocs.net/27
 # 각 행 주석 입력 후 commit
 
+import random
+import sys
+from urllib import urlopen
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    "class %%%(%%%):":
+        "클래스 %%% 를 만든다. 부모클래스 %%% 로부터 상속받는다.",
+    '''class %%%(object):
+\tdef __init__(self,***)''':
+        "클래스 %%% 는 __init__ 함수가 있고 그 매개변수는 self와 *** 이다.",
+    "*** = %%%()":
+        "*** 을(를) 클래스 %%% 의 인스턴스로 만든다.",
+    "***.***(@@@)":
+        "객체 ***로 부터 함수 *** 를 찾아 호출한다. 매개변수는 self 와 @@@이다.",
+    "***.*** = '***'":
+        "객체 ***로 부터 속성 *** 을 찾아 그 값을 '***' 으로 바꾼다",
+}
+
 
 # 각자 Study drills 시도 후 필요시 commit
 
