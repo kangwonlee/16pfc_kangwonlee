1a7c54afa0a680b6706949b14a870e65bf293d9e
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 12:27:58 +0900

    hashmap : multi line comments at the end

--- ex39/hashmap.py
+++ ex39/hashmap.py
@@ -111,6 +111,10 @@ def list(aMap):
             for k, v in bucket:
                 print(k, v)
 
-# 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
+# 여기까지 입력 후 add, commit
 
-# 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
+#  각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+#  오류노트 에 각자 오류노트 작성
