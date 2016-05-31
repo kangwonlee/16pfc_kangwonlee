ef552cf3fbcdfe4d48ff5270ea5adc8061ac43da
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 10:54:13 +0900

    ex39 print out some cities and states

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -21,6 +21,23 @@ cities = {
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
+# print out some cities
+print('-' * 10)
+print('NY State has: %s' % cities['NY'])
+print('OR State has: %s' % cities['OR'])
+
+# print some states
+print('-' * 10)
+print("Hawaii's abbreviation is: %s" % states['Hawaii'])
+print("Florida's abbreviation is: %s" % states['Florida'])
+
+# print some states
+print('-' * 10)
+print("states = %s" % states)
+print("states.items() = %s" % states.items())
+print('-' * 10)
+for state, abbrev in states.items():
+    print("%s is abbreviated %s" % (state, abbrev))
 
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
