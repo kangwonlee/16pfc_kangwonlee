66961747eeeb535681caded14c8f10e3aeddb12e
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 09:54:20 +0900

    ex38 while loop adding from more_stuff to stuff

--- ex38/ex38.py
+++ ex38/ex38.py
@@ -1,6 +1,5 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex38.html
-
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
@@ -8,6 +7,11 @@ print("Wait there are not 10 things in that list. Let's fix that.")
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
+while 10 != (len(stuff)):
+    next_one = more_stuff.pop()
+    print("Adding: %s" % next_one)
+    stuff.append(next_one)
+    print("There are %d items now." % len(stuff))
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
