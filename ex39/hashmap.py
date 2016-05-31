94bfd0b358cfae6b1664e9e004242876c47d8ada
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 12:15:31 +0900

    hashmap += list(): Prints out what's in the Map.

--- ex39/hashmap.py
+++ ex39/hashmap.py
@@ -99,6 +99,18 @@ def delete(aMap, key):
             del bucket[i]
             break
 
+
+def list(aMap):
+    """
+    Prints out what's in the Map.
+    :param aMap:
+    :return:
+    """
+    for bucket in aMap:
+        if bucket:
+            for k, v in bucket:
+                print(k, v)
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
