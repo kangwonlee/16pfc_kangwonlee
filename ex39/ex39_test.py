846166f3167284cba3effce9b0964dc7394b7521
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 12:43:44 +0900

    ex39_test : initialized states

--- ex39/ex39_test.py
+++ ex39/ex39_test.py
@@ -2,6 +2,16 @@
 # http://learnpythonthehardway.org/book/ex39.html
 # http://eclass.kpu.ac.kr/ilos/pf/course/lecture_material_view_form.acl?ARTL_NUM=373490&SCH_KEY=&SCH_VALUE=&display=10&start=1
 #  각 행 주석 입력
+import hashmap
+
+# create a mapping of states to abbreviation
+states = hashmap.new()
+hashmap.set(states, 'Oregon', 'OR')
+hashmap.set(states, 'Florida', 'FL')
+hashmap.set(states, 'California', 'CA')
+hashmap.set(states, 'New York', 'NY')
+hashmap.set(states, 'Hawaii', 'HI')
+
 
 # 여기까지 입력 후 add, commit
 
