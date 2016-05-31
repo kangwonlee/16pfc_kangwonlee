7b2d186448b68b9cd713efd437aafcf9fc719248
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 12:13:15 +0900

    hashmap +=delete(): Deletes the given key from the Map.

--- ex39/hashmap.py
+++ ex39/hashmap.py
@@ -84,6 +84,21 @@ def set(aMap, key, value):
         bucket.append((key, value))
 
 
+def delete(aMap, key):
+    """
+    Deletes the given key from the Map.
+    :param aMap:
+    :param key:
+    :return:
+    """
+    bucket = get_bucket(aMap, key)
+
+    for i in range(len(bucket)):
+        k, v = bucket[i]
+        if key == k:
+            del bucket[i]
+            break
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
