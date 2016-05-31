e5422d66b49ef7720eb536b949ca6fc744d4e977
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 09:56:23 +0900

    ex38 print items after loop

--- ex38/ex38.py
+++ ex38/ex38.py
@@ -12,6 +12,16 @@ while 10 != (len(stuff)):
     print("Adding: %s" % next_one)
     stuff.append(next_one)
     print("There are %d items now." % len(stuff))
+
+print("There we go :%s" % str(stuff))
+print("Let's do some things with stuff")
+
+print("stuff[1] = %s" % stuff[1])
+print("stuff[-1] = %s" % stuff[-1])
+print("stuff.pop() = %s" % stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
