9b0c7a3b872bdc9c28f79cb818b799fd655c6d06
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 12:43:16 +0900

    started ex39_test # http://learnpythonthehardway.org/book/ex39.html # http://eclass.kpu.ac.kr/ilos/pf/course/lecture_material_view_form.acl?ARTL_NUM=373490&SCH_KEY=&SCH_VALUE=&display=10&start=1

---
+++ ex39/ex39_test.py
@@ -0,0 +1,10 @@
+# -*-coding:utf8
+# http://learnpythonthehardway.org/book/ex39.html
+# http://eclass.kpu.ac.kr/ilos/pf/course/lecture_material_view_form.acl?ARTL_NUM=373490&SCH_KEY=&SCH_VALUE=&display=10&start=1
+#  각 행 주석 입력
+
+# 여기까지 입력 후 add, commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+#  오류노트 에 각자 오류노트 작성
