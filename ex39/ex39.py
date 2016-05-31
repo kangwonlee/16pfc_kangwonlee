fe972fd31631826d77dd1464ed50fea2eccff269
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 10:51:50 +0900

    ex39 initialize states & cities

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -1,6 +1,27 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex39.html
 
+# create a mapping of state to abbreviation
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Hawaii': 'Hi',
+}
+
+# create a basic set of states and some cities in them
+cities = {
+    'CA': 'Los Angeles',
+    'HI': 'Honolulu',
+    'FL': 'Orlando',
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
