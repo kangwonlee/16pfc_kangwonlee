3a39a5d6ff8444f6a80bb222fe69b2ca1e6f7f3b
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-31 09:51:40 +0900

    ex38 initialization

--- ex38/ex38.py
+++ ex38/ex38.py
@@ -1,6 +1,13 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex38.html
 
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
