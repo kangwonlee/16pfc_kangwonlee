b7bb007c70718ce8df24b31ad8d3e82cbd6c50b6
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 10:29:43 +0900

    CashCard_user.py += chk_bal()

--- CashCard/CashCard_user.py
+++ CashCard/CashCard_user.py
@@ -1,2 +1,14 @@
 # -*- coding: utf8 -*-
 # http://eclass.kpu.ac.kr/ilos/pf/course/lecture_material_view_form.acl?ARTL_NUM=376324&SCH_KEY=&SCH_VALUE=&display=10&start=1
+# 현금카드 모듈을 불러들임
+import CashCard as CashCard_module
+
+
+def chk_bal(message, account):
+    """
+    Print message and value
+    :param message:
+    :param account:
+    :return:
+    """
+    print("%s : %d" % (message, account.check_balance()))
