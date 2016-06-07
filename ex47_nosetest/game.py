bf78806e7d96fcc7a8f8de2240c205f0e408af67
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 12:19:46 +0900

    ex47 game += class Room

--- ex47_nosetest/game.py
+++ ex47_nosetest/game.py
@@ -1,3 +1,18 @@
 # coding: utf8
 # http://learnpythonthehardway.org/book/ex47.html
 # 각 행 주석 입력 후 commit
+
+
+class Room(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.paths = {}
+
+    def go(self, directions):
+        return self.paths.get(directions, None)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
+
+# end class Room
