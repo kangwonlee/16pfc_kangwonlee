849222d8fe4742aacaad29010f60b2bc4d0a7df5
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 11:19:29 +0900

    CashCardClass __main__ : create object, deposit(10000), chk_bal()

--- CashCard/CashCardClass.py
+++ CashCard/CashCardClass.py
@@ -47,3 +47,15 @@ class SimpleCashCard:
         return self.balance_won
 
 # SimpleCashCard 클래스 정의 끝
+
+# 아래의 내용은 이 파일이 import 될 때는 실행되지 않음
+if "__main__" == __name__:
+    # 모듈 실습 함수를 하나 사용할 수 있게 함
+    from CashCard_user import chk_bal
+
+    # myCard 객체를 SimpleCashCard 클래스에 정한 대로 만듦
+    myCard = SimpleCashCard()
+
+    # myCard 에 10000원 입금
+    myCard.deposit(10000)
+    chk_bal("입금 후 잔고 확인", myCard)
