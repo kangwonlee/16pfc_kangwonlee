7fd6b3ca32fa3f542f4c2f4e82ce437be94908f2
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 12:23:22 +0900

    ex47 nose tests script += test_room()

--- ex47_nosetest/ex47_nose_tests.py
+++ ex47_nosetest/ex47_nose_tests.py
@@ -1,3 +1,15 @@
 # coding: utf8
 # http://learnpythonthehardway.org/book/ex47.html
 # 각 행 주석 입력 후 commit
+
+from nose.tools import *
+
+from game import Room
+
+
+def test_room():
+    gold = Room("GoldRoom",
+                """This room has gold in it you can grab. There's a door to the north.""")
+
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
