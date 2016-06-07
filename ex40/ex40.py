31861aa1edcd37698298a4620080c0d23d179269
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 10:03:20 +0900

    ex40 class Song

--- ex40/ex40.py
+++ ex40/ex40.py
@@ -23,3 +23,13 @@ class MyStuff(object):
 thing = MyStuff()
 thing.apple()
 print(thing.tangerine)
+
+
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_a_song(self):
+        for line in self.lyrics:
+            print(line)
+# end class Song
