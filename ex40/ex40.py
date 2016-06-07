95d1426b5988efd05955cde7d0b0b9ba59071a3f
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 10:04:30 +0900

    ex40 happy_bday as in instance of class Song

--- ex40/ex40.py
+++ ex40/ex40.py
@@ -33,3 +33,7 @@ class Song(object):
         for line in self.lyrics:
             print(line)
 # end class Song
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
