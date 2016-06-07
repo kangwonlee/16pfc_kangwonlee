26c9ebd6f684283782dfd4a33d11e6f5f400b5e2
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 11:11:14 +0900

    CashCardClass class SimpleCashCard : += constructor

--- CashCard/CashCardClass.py
+++ CashCard/CashCardClass.py
@@ -1,2 +1,16 @@
 # -*- coding: utf8 -*-
 # http://eclass.kpu.ac.kr/ilos/pf/course/lecture_material_view_form.acl?ARTL_NUM=376324&SCH_KEY=&SCH_VALUE=&display=10&start=1
+
+
+class SimpleCashCard:
+    """
+    SimpleCashCard class
+    deposit, withdraw, and check_balance methods
+    """
+
+    def __init__(self):
+        print("SimpleCashCard __init__()")  # 함수 호출 표식
+        # 클래스 생성자 (컨스트럭터)
+        # 멤버 변수 초기화
+        # 각 카드 별로 따로 잔고를 기록한다
+        self.balance_won = 0
