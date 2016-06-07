bbf0e9f09edb3801bb41a19abecd695159965cea
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 12:39:16 +0900

    too long one line -> two lines

--- ex47_nosetest/ex47_nose_tests.py
+++ ex47_nosetest/ex47_nose_tests.py
@@ -12,7 +12,8 @@ from game import Room
 
 def test_room():
     gold = Room("GoldRoom",
-                """This room has gold in it you can grab. There's a door to the north.""")
+                """This room has gold in it you can grab."""
+                """ There's a door to the north.""")
 
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
