a587793bbc9020991bad35a3157190c9e62d5700
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-07 10:21:17 +0900

    module CashCard.py : deposit()

--- CashCard/CashCard.py
+++ CashCard/CashCard.py
@@ -1,1 +1,18 @@
 # -*- coding: utf8 -*-
+balance_won = 0 # 현금카드 잔고
+
+
+# 현금 카드로 할 수 있는 것: 입금, 춞금, 잔고 확인
+
+def deposit(amount_won):
+    """
+    Deposit some amout of mony into cash card
+    :param amount_won:
+    :return:
+    """
+    # deposit 함수 안의 balance_won 이
+    # 전역변수임을 표시
+    global balance_won
+
+    # 입금하면 잔고가 증가한다
+    balance_won += amount_won
